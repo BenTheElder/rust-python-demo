2763827852ed475355baa6f85a5ae2215c4b3f25
BenTheElder <ben.the.elder@gmail.com>
2014-12-23 23:18:55 -0500

    Clean up project a bit, pass structure from rust to python.
    Also add comments to python source.

--- demo.py
+++ demo.py
@@ -3,8 +3,10 @@ import sys
 import glob
 from ctypes import *
 
+#get path to this file.
 path = os.path.dirname(os.path.realpath(__file__))
 
+#determine dynamic library extension based on platform
 ext = ""
 if sys.platform == "linux" or sys.platform == "linux2":
 	ext = ".so"
@@ -16,17 +18,43 @@ else:
 	print("Error: Unrecognized Platform.")
 	sys.exit(-1)
 
+#find rust library
 f = glob.glob("%s/target/*%s"%(path,ext))
 if not f:
 	print("Error: Failed to find rust library. You may need to run `cargo build` first.")
 	sys.exit(-1)
 
+#load the library
 demo = cdll.LoadLibrary(f[0])
 
-pystr = "(And Python!)"
-c_s = c_char_p(pystr)
+#create a python string
+py_string = "(And Python!)"
+#convert to a ctypes c style string (null terminated char array pointer)
+c_string = c_char_p(py_string)
 
-hello_world = demo.hello_world
-hello_world.restype = c_char_p
-hello_world.argtypes = [c_char_p]
-print("%s" % hello_world(c_s))
+#set the result type for the hello_world method to a c style string
+demo.hello_world.restype = c_char_p
+#set the arguments for hello_world to a c style string
+demo.hello_world.argtypes = [c_char_p]
+
+#call hello_world and print the result
+print("demo.hello_world():\n\"%s\"" % demo.hello_world(c_string))
+
+
+print("")
+
+
+#create class to represent the test structure in the library
+class DemoStruct(Structure):
+	_fields_ = [("a", c_int), ("b", c_int)]
+
+#set the result type for the test method
+demo.get_demo_struct.restype = POINTER(DemoStruct)
+
+#call get_demo_struct and keep the returned pointer
+p_demo_struct = demo.get_demo_struct()
+#get the struct behind the pointer
+demo_struct = p_demo_struct.contents
+#print the contents.
+print("demo.get_demo_struct():\n{a: %d,b: %d}"%(demo_struct.a, demo_struct.b))
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 extern crate libc;
 use std::c_str::CString;
 
-
-fn world() -> String {
-	"world.".to_string()
+#[repr(C)]
+pub struct DemoStruct {
+	a: i32,
+	b: i32
 }
+
+
+#[no_mangle]
+pub extern fn get_demo_struct() -> *mut DemoStruct {
+	&mut DemoStruct{a: 0, b: 1337}
+}
+
 
 #[no_mangle]
 pub extern fn hello_world(c: *const libc::c_char) -> *const libc::c_char {
-	unsafe{
+	let mut s = "Hello world (From rust). ".to_string();
+	unsafe {
 		let cs = CString::new(c, false);
-		let mut s = "Hello ".to_string() + world().as_slice() + " (From rust) ";
 		s = s + cs.as_str().unwrap();
 		s.to_c_str().into_inner()
 	}
